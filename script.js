ad9ca5c1be3162ac8828f4cb3c6fc57f5c4f650b
nahid <nahidthedeveloper@gmail.com>
2024-12-31 19:10:25 +0600

    add about page and other animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Lenis Start
 document.addEventListener("DOMContentLoaded", () => {
     const lenis = new Lenis({
         lerp: 0.05, // Adjust smoothness level
@@ -10,3 +11,72 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     requestAnimationFrame(raf);
 });
+// Lenis End
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('nav', {
+    opacity: 0,
+    duration: 2,
+    ease: 'power2.out'
+})
+
+gsap.to('#nav', {
+    opacity: 0,
+    ease: 'power2.out',
+    scrollTrigger: {
+        scroller: document.body,
+        trigger: '#nav',
+        start: 'top 2%',
+        end: 'top 5%',
+        scrub: 1,
+        // markers: true,
+    }
+});
+
+gsap.from("#firstHR", {
+    width: '10%',
+    duration: 2,
+    ease: 'power2.out',
+})
+
+gsap.from('.section1_left h1, .section1_left svg', {
+    y: 300,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.2,
+    ease: 'power2.out'
+})
+
+gsap.from('footer a', {
+    y: 200,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.2,
+    ease: 'power2.out',
+    scrollTrigger: {
+        trigger: 'footer div',
+        start: 'top 95%',
+        end: 'top 85%',
+        once: true,
+        // markers: true,
+    }
+});
+
+const aboutTimeline = gsap.timeline({ paused: true });
+
+aboutTimeline
+  .to('.overlay', {
+    width: 0,
+    duration: 1,
+  }, 'a')
+  .from('#about_men img', {
+    opacity: 0,
+    scale: 1.5,
+    duration: 1,
+  }, 'a');
+
+aboutTimeline.play();
+
+
+
